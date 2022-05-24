101bf3a98962de69f0de5b77a3269a07ec868991
Vincent GG <centgint@gmail.com>
2022-05-24 11:21:50 +0700

    Update PopUpBlackWidow.java

--- src/PopUpBlackWidow.java
+++ src/PopUpBlackWidow.java
@@ -94,8 +94,6 @@ public class PopUpBlackWidow extends javax.swing.JFrame {
     }//GEN-LAST:event_TblBeliActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        Beranda main = new Beranda();
-        main.setVisible(true);
         dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
