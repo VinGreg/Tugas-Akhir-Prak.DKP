0dcd0ee99c5374e0741d121b4d90937e67373393
Vincent GG <centgint@gmail.com>
2022-05-24 11:24:52 +0700

    Update PopUpPool.java

--- src/PopUpPool.java
+++ src/PopUpPool.java
@@ -94,8 +94,6 @@ public class PopUpPool extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        Beranda main = new Beranda();
-        main.setVisible(true);
         dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
