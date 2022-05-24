d71ca05269d5d7827932042017460ac6fd701db8
Vincent GG <centgint@gmail.com>
2022-05-24 11:22:59 +0700

    Update PopUpLostCity.java

--- src/PopUpLostCity.java
+++ src/PopUpLostCity.java
@@ -94,8 +94,6 @@ public class PopUpLostCity extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        Beranda main = new Beranda();
-        main.setVisible(true);
         dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
